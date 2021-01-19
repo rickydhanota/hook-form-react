06b3a0b80137f095bfaf910041619349ee3b3b96
Ricky <rickdhanota@gmail.com>
2021-01-18 23:41:15 -0800

    created a hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './FormComponent';
+import FormComponent from "./FormComponent";
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+
+      <FormComponent />
     </div>
   );
 }
--- src/FormComponent.js
+++ src/FormComponent.js
@@ -6,9 +6,44 @@ const FormComponent = props => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const createUser = (e) => {
+        e.preventDefault()
+        const newUser = {firstName, lastName, email, password};
+        console.log(`${newUser}, created!`);
+        setFirstName("");
+        setLastName("");
+        setPassword("");
+        setEmail("");
+    }
+
     return (
         <div>
-            
+            <form onSubmit = {createUser}>
+                <div className = "firstName">
+                    <label>First Name</label>
+                    <input type = "text" onChange = {(e) => setFirstName(e.target.value)}  value = {firstName} />
+                </div>
+
+                <div className = "lastName">
+                    <label>Last Name</label>
+                    <input type = "text" onChange = {(e) => setLastName(e.target.value)} value = {lastName} ></input>
+                </div>
+
+                <div className = "email">
+                    <label>Email</label>
+                    <input type = "text" onChange = {(e) => setEmail(e.target.value)} value = {email} ></input>
+                </div>
+
+                <div className = "password">
+                    <label>Password</label>
+                    <input type = "text" onChange = {(e) => setPassword(e.target.value)} value = {password} ></input>
+                </div>
+
+                <div className = "submitButton">
+                    <input type = "submit" value = "Create User" />
+                </div>
+
+            </form>
         </div>
     );
 };
