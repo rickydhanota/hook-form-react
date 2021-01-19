2c94ee91a51913efce3f44b9626ffbc4af31bc98
Ricky <rickdhanota@gmail.com>
2021-01-18 22:57:54 -0800

    creating components

---
+++ src/FormComponent.js
@@ -0,0 +1,11 @@
+import React, { Component, useState } from 'react';
+
+export default class FormComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
